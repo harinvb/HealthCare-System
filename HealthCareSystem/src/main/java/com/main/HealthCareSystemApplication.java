d5f00f3534170d71b748360d352e007aee0c695b
harinvb <harinvb@users.noreply.github.com>
2021-03-17 20:29:47 +0530

    Project BluePrint

---
+++ HealthCareSystem/src/main/java/com/main/HealthCareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareSystemApplication.class, args);
+	}
+
+}
