b7f04f6af67534327575940266863638fe68376b
harinvb <harinvb@gmail.com>
2021-03-22 10:58:30 +0530

    Update Appointment.java

--- HealthCareSystem/src/main/java/com/cg/healthcare/entities/Appointment.java
+++ HealthCareSystem/src/main/java/com/cg/healthcare/entities/Appointment.java
@@ -5,16 +5,60 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GeneratorType;
+
 import com.cg.healthcare.exception.InvalidAppointmentStatusException;
 
 public class Appointment implements Serializable{
 	
+	@Id
+	@GeneratedValue
 	private int id;
 	private LocalDate appointmentDate;
-	private AppointmentStatus approvalStatus;  
+	private AppointmentStatus approvalStatus;
 	private Set<DiagnosticTest> diagnosticTests; 
 	private Patient patient;
 	private DiagnosticCenter diagnosticCenter;
+	public LocalDate getAppointmentDate() {
+		return appointmentDate;
+	}
+	public void setAppointmentDate(LocalDate appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+	public AppointmentStatus getApprovalStatus() {
+		return approvalStatus;
+	}
+	public void setApprovalStatus(AppointmentStatus approvalStatus) {
+		this.approvalStatus = approvalStatus;
+	}
+	public Set<DiagnosticTest> getDiagnosticTests() {
+		return diagnosticTests;
+	}
+	public void setDiagnosticTests(Set<DiagnosticTest> diagnosticTests) {
+		this.diagnosticTests = diagnosticTests;
+	}
+	public Patient getPatient() {
+		return patient;
+	}
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+	public DiagnosticCenter getDiagnosticCenter() {
+		return diagnosticCenter;
+	}
+	public void setDiagnosticCenter(DiagnosticCenter diagnosticCenter) {
+		this.diagnosticCenter = diagnosticCenter;
+	}
+	public Set<TestResult> getTestResult() {
+		return testResult;
+	}
+	public void setTestResult(Set<TestResult> testResult) {
+		this.testResult = testResult;
+	}
 	private Set<TestResult> testResult;
 	
 }
