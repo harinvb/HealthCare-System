0f4ac48b89df44fb61a49a0563ae209021abae3c
Karthik1-hub <karthiksatya121@gmail.com>
2021-03-22 12:10:14 +0530

    hjj

---
+++ HealthCareSystem/src/main/java/com/cg/healthcare/service/ITestServiceImpl.java
@@ -0,0 +1,40 @@
+package com.cg.healthcare.service;
+
+import java.util.Set;
+
+import com.cg.healthcare.entities.DiagnosticCenter;
+import com.cg.healthcare.entities.DiagnosticTest;
+
+public class ITestServiceImpl implements ITestService{
+
+	@Override
+	public DiagnosticTest addTest(DiagnosticTest test) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DiagnosticTest updateTest(DiagnosticTest test) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DiagnosticTest removeTest(DiagnosticTest test) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<DiagnosticTest> viewAllTest(DiagnosticTest test) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DiagnosticTest addTestInCenter(DiagnosticCenter center) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
