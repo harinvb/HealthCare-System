a48cd6bdd6e82979b4e754d12b2b63f95a395b7b
thenameispavan <thenameispavan3@gmail.com>
2021-03-23 16:00:28 +0530

    controller

--- HealthCareSystem/src/main/java/com/cg/healthcare/controller/HealthCareController.java
+++ HealthCareSystem/src/main/java/com/cg/healthcare/controller/HealthCareController.java
@@ -4,21 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.healthcare.entities.Appointment;
+import com.cg.healthcare.entities.DiagnosticCenter;
+import com.cg.healthcare.entities.DiagnosticTest;
 import com.cg.healthcare.service.AppointmentService;
+import com.cg.healthcare.service.IDiagnosticCenterService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/healthcareapp")
 public class HealthCareController {
 	
 	@Autowired
 	AppointmentService appserv;
-	
+	IDiagnosticCenterService centerService;
 	@PostMapping
 	public Appointment getAppointments(@RequestBody Appointment appointment) throws Exception {
 		return appserv.addAppointment(appointment);
@@ -27,5 +32,30 @@ public class HealthCareController {
 	@GetMapping
 	public List<Appointment> get() {
 		return appserv.get();
+	}
+	@RequestMapping
+	public List<DiagnosticCenter> getDiagnosticCenters(){
+		return centerService.getAllDiagnosticCenters();
+	}
+	@PostMapping("/addCenter")
+	public DiagnosticCenter addDiagnosticCenter(@RequestBody DiagnosticCenter diagnosticCenter) throws Exception {
+		return centerService.addDiagnosticCenter(diagnosticCenter);
+		
+	}
+	@GetMapping("/{diagnosticCenterId}")
+	public DiagnosticCenter getDiagnosticCenterById(@PathVariable int diagnosticCenterId) {
+		return centerService.getDiagnosticCenterById(diagnosticCenterId);
+	}
+	@PutMapping
+	public DiagnosticCenter updateDiagnosticCenter(@RequestBody DiagnosticCenter diagnosticCenter) {
+		return centerService.updateDiagnosticCenter(diagnosticCenter);
+	}
+	@GetMapping("/viewTestDetails")
+	public DiagnosticTest viewTestDetails(@PathVariable int diagnosticCenterId,String testName) {
+		return centerService.viewTestDetails(diagnosticCenterId, testName);
+	}
+	@PostMapping("/addTest")
+	public DiagnosticTest addTest(int diagnosticcenterId, int testid) {
+		return centerService.addTest(diagnosticcenterId, testid);
 	}
 }
