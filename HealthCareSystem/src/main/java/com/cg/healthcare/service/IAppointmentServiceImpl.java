8ecd4af42ad7b629ae70a9157337079c0a061242
harinvb <harinvb@gmail.com>
2021-03-25 14:26:11 +0530

    Update IAppointmentServiceImpl.java

--- HealthCareSystem/src/main/java/com/cg/healthcare/service/IAppointmentServiceImpl.java
+++ HealthCareSystem/src/main/java/com/cg/healthcare/service/IAppointmentServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.cg.healthcare.dao.IAppointmentRepository;
 import com.cg.healthcare.dao.IDiagnosticCenterRepositoryInt;
 import com.cg.healthcare.dao.IDiagnosticTestRepository;
