3e36a0e3439cf7ad8ff7ab4d6e2f350719a33834
harinvb <harinvb@gmail.com>
2021-03-26 09:08:49 +0530

    AbstractTest

---
+++ HealthCareSystem/src/test/java/com/main/AbstractTest.java
@@ -0,0 +1,37 @@
+package com.main;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebAppConfiguration
+public abstract class AbstractTest {
+	protected MockMvc mvc;
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	protected void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	protected String mapToJson(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+
+	protected <T> T mapFromJson(String json, Class<T> clazz)
+			throws JsonParseException, JsonMappingException, IOException {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+}
---
+++ HealthCareSystem/src/test/java/com/main/HealthCareSystemApplicationTests.java
@@ -0,0 +1,31 @@
+package com.main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.cg.healthcare.entities.DiagnosticCenter;
+
+@SpringBootTest
+class HealthCareSystemApplicationTests extends AbstractTest {
+	@Override
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Test
+	public void getDiagnosticCenter() throws Exception {
+		String uri = "/DiagnosticCenter/getDiagnosticCenter/2";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+		DiagnosticCenter dc = super.mapFromJson(content, DiagnosticCenter.class);
+		assertEquals(2, dc.getDiagonasticCenterid());
+	}
+
+}
