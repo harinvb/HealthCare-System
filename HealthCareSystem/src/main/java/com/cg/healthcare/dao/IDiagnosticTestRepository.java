ba4098ed61d81ca5555041fc28c94c0a2d66d1a7
Karthik1-hub <karthiksatya121@gmail.com>
2021-04-09 10:47:01 +0530

    Update IDiagnosticTestRepository.java

--- HealthCareSystem/src/main/java/com/cg/healthcare/dao/IDiagnosticTestRepository.java
+++ HealthCareSystem/src/main/java/com/cg/healthcare/dao/IDiagnosticTestRepository.java
@@ -3,6 +3,12 @@ package com.cg.healthcare.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+/************************************************************************************
+ *@author          	K S S Karthik
+ *Description      	It is a Data Access Object Interface that provides methods for Implementation class.  
+ *Version          	1.0
+ *Created Date    	30-MAR-2021
+ ************************************************************************************/
 //import org.springframework.data.jpa.repository.Query;
 
 import com.cg.healthcare.entities.DiagnosticTest;
