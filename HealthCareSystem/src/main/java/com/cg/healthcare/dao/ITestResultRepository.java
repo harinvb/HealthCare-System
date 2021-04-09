e04d4b64b3fe96303cc4b7900c63ab4d89785752
Karthik1-hub <karthiksatya121@gmail.com>
2021-04-09 10:46:45 +0530

    Update ITestResultRepository.java
    hh

--- HealthCareSystem/src/main/java/com/cg/healthcare/dao/ITestResultRepository.java
+++ HealthCareSystem/src/main/java/com/cg/healthcare/dao/ITestResultRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //
 //import com.cg.healthcare.entities.Patient;
 import com.cg.healthcare.entities.TestResult;
-
+/************************************************************************************
+ *@author          	K S S Karthik
+ *Description      	It is a Data Access Object Interface that provides methods for Implementation class.  
+ *Version          	1.0
+ *Created Date    	30-MAR-2021
+ ************************************************************************************/
 public interface ITestResultRepository extends JpaRepository<TestResult, Integer>{
 
 }
